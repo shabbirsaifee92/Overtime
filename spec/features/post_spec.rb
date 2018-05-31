d96f524247e3b8f8de780c9b5ac9a364b8920dfa
shabbir <ssaifee@covermymeds.com>
2018-05-31 13:44:54 -0400

    updated seed file with default post content

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 describe 'navigation' do
   describe 'index' do
-    it 'can be reached' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
     end
   end
