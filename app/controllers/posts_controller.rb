4b3624c5c95785bd06b3ec12cfee9d4072eae4cf
shabbir <ssaifee@covermymeds.com>
2018-05-31 14:34:28 -0400

    implemented creation of post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,33 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show]
 
   def index
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: 'New post created successfully'
+    else
+      flash[:error] = 'Cannot create new post!!'
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:date, :rationale)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
