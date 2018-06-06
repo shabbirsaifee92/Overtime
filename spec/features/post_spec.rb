06412c643e8a63d1d9057e5e8e6ec720d22f4a90
shabbir <ssaifee@covermymeds.com>
2018-06-06 00:07:24 -0400

    implement add new post

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -27,6 +27,15 @@ describe 'navigation' do
     end
   end
 
+  describe 'new' do
+    it 'has link from homepage' do
+      visit root_path
+      click_link('new_post_tab')
+      expect(page.status_code).to eq(200)
+    end
+
+  end
+
   describe 'creation' do
     before do
       visit new_post_path
