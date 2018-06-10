f62b49b2006f38fd1f82f71f575ce36196f69dd4
shabbir <ssaifee@covermymeds.com>
2018-06-09 22:55:34 -0400

    add status field to admin dashboard for post

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
-user = User.create(email: '123@test.com', password: 'password', password_confirmation: 'password', first_name: 'shabbir', last_name: 'saifee')
+user = User.create(email: '123@test.com', password: 'password', password_confirmation: 'password', first_name: 'test', last_name: 'user')
 
 puts "1 user  been create--------------------------"
+
+User.create!(email: 'shabbirsaifee91@gmail.com', password: 'password', password_confirmation: 'password', first_name: 'shabbir', last_name: 'saifee', type: 'AdminUser')
+
+puts '1 Admin User created---------------------------'
+
 100.times do |post|
   Post.create(user: user, date: Date.today, rationale: "#{post} rational content")
 end
