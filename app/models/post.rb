3fb17e79ccbffb3e549022c853896d07f9447e94
shabbir <ssaifee@covermymeds.com>
2018-06-09 23:23:00 -0400

    implemented edit functionality for post status

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   validates :date, :rationale, presence: true
   belongs_to :user
 
-  enum status: { default: 0, submitted: 1, approved: 2, rejected: 3}
+  enum status: { default: 0, submitted: 1, approved: 2, rejected: 3 }
 end
