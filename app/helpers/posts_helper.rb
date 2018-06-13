6c1d229c4d02a4a6fd1082b0a8ff2c20f3bb04ec
shabbir <ssaifee@covermymeds.com>
2018-06-13 01:12:18 -0400

    update post index to show status and disable edit link for approved posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,21 @@
 module PostsHelper
+
+  def status_label(status)
+    status_span_generator status
+  end
+
+  private
+
+  def status_span_generator(status)
+    case status
+    when 'default'
+      content_tag :span, 'Unsubmitted', class: 'label label-primary'
+    when 'submitted'
+      content_tag :span, 'Pending', class: 'label label-warning'
+    when 'approved'
+      content_tag :span, status.titleize, class: 'label label-success'
+    when 'rejected'
+      content_tag :span, status.titleize, class: 'label label-danger'
+    end
+  end
 end
