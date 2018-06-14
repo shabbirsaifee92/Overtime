1df3246a518c47fbf0ea1a63d55e41f0e7f6415a
shabbir <ssaifee@covermymeds.com>
2018-06-13 23:14:51 -0400

    added stub sms tool for testing

---
+++ lib/stubbed_sms_tool.rb
@@ -0,0 +1,13 @@
+module StubbedSmsTool
+
+  Message =  Struct.new(:num, :msg)
+  @messages = []
+
+  def self.send_sms(num, msg)
+    @messages << Message.new(num, msg)
+  end
+
+  def self.messages
+    @messages
+  end
+end
