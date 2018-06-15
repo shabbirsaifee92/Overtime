b05c26e777d55c7d246b52ceeab46a5c47da0c19
shabbir <ssaifee@covermymeds.com>
2018-06-15 11:43:39 -0400

    Send first sms using twilio

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -3,5 +3,16 @@ module SmsTool
   def self.send_sms(number:, message:)
     puts 'Sending SMS......'
     puts "#{message} #{number}"
+
+    account_sid = ENV['TWILIO_ACCOUNT_SID']
+    auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    @client.messages.create(
+                       from: ENV['TWILIO_PHONE_NUMBER'],
+                       to: "+91#{number}",
+                       body: "#{message}"
+    )
   end
 end
