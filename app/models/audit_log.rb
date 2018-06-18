2b5eba45ee3feaea6bd31590fab7a2626188ed73
shabbir <ssaifee@covermymeds.com>
2018-06-18 00:48:29 -0400

    tests for audit log model

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,4 +1,13 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
+  validates :start_date, :status, presence: true
 
+  after_initialize :set_defaults
+
+
+  private
+
+  def set_defaults
+    self.start_date ||= Date.today - 6.days
+  end
 end
--- spec/models/audit_log_spec.rb
+++ spec/models/audit_log_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe AuditLog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:audit_log) { FactoryBot.create(:audit_log) }
+
+  describe 'creation' do
+    it 'can be created' do
+      expect(audit_log).to be_valid
+    end
+  end
+
+  describe 'validations' do
+    it 'should have user associate with it' do
+      expect(audit_log.user).to be_a_kind_of(User)
+    end
+
+    it 'should have a start date' do
+      audit_log.start_date = nil
+      expect(audit_log).to_not be_valid
+    end
+
+    it 'should have a start date 6 days prior' do
+      expect(audit_log.start_date).to eq(Date.today - 6.days)
+    end
+
+    it 'should have a status' do
+      audit_log.status = nil
+      expect(audit_log).to_not be_valid
+    end
+  end
 end
