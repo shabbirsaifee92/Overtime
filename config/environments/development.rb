1f270f4e90d19d54d57aa420e6dc3c9e000a7c9a
shabbir <ssaifee@covermymeds.com>
2018-06-26 19:43:11 -0400

    implemented basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,6 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
@@ -53,4 +52,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 5555 }
 
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
 end
