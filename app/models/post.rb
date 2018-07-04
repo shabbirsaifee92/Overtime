6542efa6076ba7028dff01e8f31b918aafe4bba6
shabbir <ssaifee@covermymeds.com>
2018-07-04 16:10:57 -0400

    update end_date for audit log

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,11 @@ class Post < ApplicationRecord
   enum status: { default: 0, submitted: 1, approved: 2, rejected: 3 }
 
   scope :posts_by, ->(user) { where(user: user) }
+  # after_save :update_audit_log
 
   def update_audit_log
     audit_log = AuditLog.where(user: self.user, start_date: (self.date - 7.days..self.date)).last
+    audit_log.update_attribute(:end_date, Date.today()) if self.submitted!
     audit_log.try(:confirmed!)
   end
 end
