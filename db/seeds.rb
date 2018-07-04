6bce47cca892ede4b46dafa0d04861f02d519fdc
shabbir <ssaifee@covermymeds.com>
2018-07-04 13:08:56 -0400

    update name for email to managers

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,10 +20,10 @@ puts '1 Admin User created---------------------------'
   Post.create(user: employee, date: Date.today, rationale: "#{post} rational content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", hours: 2.5)
 end
 
-puts "100 posts have been create--------------------------"
+puts "10 posts have been create--------------------------"
 
 AuditLog.create(user: employee, start_date: Date.today - 6.days)
 AuditLog.create(user: employee, start_date: Date.today - 13.days)
 AuditLog.create(user: employee, start_date: Date.today - 20.days)
 
-puts "100 audit logs have been create--------------------------"
+puts "3 audit logs have been create--------------------------"
