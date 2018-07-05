2b869d6a3c769932a0fd90aed458539defe177dd
shabbir <ssaifee@covermymeds.com>
2018-07-05 18:52:09 -0400

    only allow admin user to access admin dashboard

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -11,17 +11,10 @@ module Admin
     before_action :authenticate_admin
 
     def authenticate_admin
-      unless current_user.admin?
+      unless current_user.type == 'AdminUser'
         flash[:error] = 'Page you are looking for does not exist'
         redirect_to root_path
       end
-
     end
-
-    # Override this value to specify the number of elements to display at a time
-    # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
   end
 end
