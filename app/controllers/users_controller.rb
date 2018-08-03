924768e2fc6fa7f1b0a1455e5caaef524bffe1bf
shabbir <ssaifee@covermymeds.com>
2018-08-03 12:23:20 -0400

    implement following and followers page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,15 @@ class UsersController < ApplicationController
   end
 
   def follow
-    new_following = Employee.find(params[:id])
-    new_follower = UserFollower.new(user: new_following, follower: current_user)
-    if new_follower.save
-      redirect_to root_path, notice: "You are now following #{new_following.full_name}"
+    new_followed_user = Employee.find(params[:id])
+    if current_user.follow!(new_followed_user)
+      redirect_to root_path, notice: "You are now following #{new_followed_user.full_name}"
     else
       render :index, error: "Unable to add the follower. Please try later"
     end
   end
+
+  def followings; end
+
+  def followers ; end
 end
